84e4609f98488c5199dfa33540bb9846df310f9c
xgd <854338939@qq.com>
2023-01-31 22:30:48 +0800

    文章模块静态页面初步开发

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,20 @@
 <template>
-  <Tutorial/>
+  <div>
+    <articleTab></articleTab>
+    <articleList></articleList>
+  </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-
+import Vue from "vue";
+import articleTab from "../components/Article/articleTab.vue";
+import articleList from "../components/Article/articleList.vue";
+import articelItem from "../components/Article/articleItem.vue";
 export default Vue.extend({
-  name: 'IndexPage'
-})
+  components: { articleTab,articleList,articelItem},
+  name: "IndexPage",
+});
 </script>
+<style lang="less" scoped >
+
+</style>
