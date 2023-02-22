38f31f6b462dc5e442fea17a2fb6a9d6fc914bc4
xgd <854338939@qq.com>
2023-02-22 23:02:23 +0800

    加入tag

--- pages/Tabs/tabs.vue
+++ pages/Tabs/tabs.vue
@@ -1,21 +1,28 @@
 <template>
   <div class="Tab">
+    <!-- <div class="tag">码上掘金</div> -->
     <img
       src="../../assets/img/logo.png"
       ></img>
-    <el-tabs
-      class="tabs"
-      v-model="activeName"
-      @tab-click="handleClick"
-    >
-      <el-tab-pane
-        class="pane"
-        v-for="(item, index) in headers"
-        :key="item.id"
-        :label="item.tab"
-        :name="item.tab"
-      ></el-tab-pane>
-    </el-tabs>
+      <el-tabs
+        class="tabs"
+        v-model="activeName"
+        @tab-click="handleClick"
+      >
+       
+        <el-tab-pane
+          class="pane"
+          v-for="(item, index) in headers"
+          :key="item.id"
+          :label="item.tab"
+          :name="item.tab"
+        >
+        <span slot="label"> 
+          <el-badge :value="item.tag"  style="margin:5px 40px 0px 0px;"><span >{{item.tab}}</span></el-badge>
+        </span>
+        </el-tab-pane>
+        </el-badge>
+      </el-tabs>
     <i class="el-icon-sunny"></i>
   </div>
 </template>
@@ -53,6 +60,10 @@ export default {
 /deep/.el-tabs__nav-wrap::after {
   height: 0 !important;
 }
+/* 去掉el-tab-pane切换时的蓝色下划线*/
+/deep/ .el-tabs__active-bar {
+  background-color: transparent;
+}
 /* 修复el-tabs意外出现蓝色边框问题bug*/
 ::v-deep .el-tabs__item {
   box-shadow: none !important;
@@ -61,21 +72,28 @@ export default {
 .Tab {
   background-color: white;
   margin: auto;
-  height: 55px;
+  height:60px;
   border-top: 0.5px solid #86909c2f;
   border-bottom: 0.5px solid #86909c2f;
   box-shadow: 0px 1px 1px 0px #86909c2f;
   display: flex;
   align-content: center;
   padding-top:0.5%;
+  .tag{
+    position: absolute;
+    background-color: red;
+    color: white;
+    border-radius: 20px;
+    left:39%;
+  }
   .tabs {
     width: 90%;
-    margin-left:1%;
+    margin-left:3%;
   }
   img{
-    margin-top:0.5%;
+    margin-top:1%;
     width: 10%;
-    height: 70%;
+    height: 60%;
   }
   i{
     margin: 1% 1% 0 0;
@@ -86,7 +104,7 @@ export default {
   .Tab img{
     width: 15%;
     height: 70%;
-    margin-top:0.5%;
+    margin-top:1%;
   }
    .Tab .tabs{
     width: 70%;
